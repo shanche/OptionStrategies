1cb5907e78a59accd4bcaed78f5b00303fb76a5f
Mountain <yzhao0527@users.noreply.github.com>
2016-11-01 10:35:09 -0400

    Create setup.py

---
+++ python-package/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='qlib',
+      version='0.1',
+      description='A Python Package for Quantitative Financial Research',
+      author='Yue Zhao',
+      author_email='do-not-email@me.com',
+      packages=['qlib',
+                'qlib.web'],
+      )
